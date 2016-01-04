b5ba658f5d9b30db02cfa6f64b12a7259835d99f
Sean Purcell <iburinoc@gmail.com>
2016-01-04 16:09:25 -0500

    added basic background loader implementation

---
+++ bgserver/bgserver.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import sys
+import os
+import random
+
+from flask import Flask, send_file
+
+if len(sys.argv) < 2:
+	print 'Usage: %s <root_dir>' % sys.argv[0]
+	sys.exit(1)
+
+app = Flask(__name__)
+
+root_dir = sys.argv[1]
+files = [f for f in os.listdir(root_dir) if ('.jpg' in f or '.png' in f)]
+if len(files) == 0:
+	print 'Must be at least one image in dir'
+	sys.exit(1)
+
+print '%d files to select from' % len(files)
+
+@app.route('/')
+def bgimage():
+	f = random.choice(files)
+	mimetype = 'image/png' if '.png' in f else 'image/jpeg'
+	return send_file(f, mimetype=mimetype)
+
+if __name__ == '__main__':
+	app.run()
+
