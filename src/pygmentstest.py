758b19791727ca2007c6b84c61988d7af336e6cb
Sean Purcell <iburinoc@gmail.com>
2016-01-05 21:59:15 -0500

    cleaned up test program for merging

--- src/pygmentstest.py
+++ src/pygmentstest.py
@@ -4,7 +4,6 @@ import pygments
 from pygments import lexers
 from pygments import styles
 
-from blurformatter import BlurFormatter
 from pygments.formatters import ImageFormatter, JpgImageFormatter
 
 from PIL import ImageFilter
@@ -17,17 +16,17 @@ text = ''
 with open(sys.argv[1], 'r') as f:
 	text = f.read()
 
+text = text + ' ' * 160 + '\n'
+
 lexer = lexers.get_lexer_for_filename(sys.argv[1])
 style = styles.get_style_by_name('manni')
 opts = {
 	"style": style,
 	"line_numbers": False,
 	"font_name": "Inconsolata",
-	"blur_radius": 3,
 	"font_size": 8,
 }
-formatter = BlurFormatter(**opts)
+formatter = ImageFormatter(**opts)
 
-#print formatter.get_style_defs()
-img = formatter.format(lexer.get_tokens(text), "out.jpg")
+img = formatter.format(lexer.get_tokens(text), "out.png")
 
