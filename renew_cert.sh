7aa8b92f6f0171703d29ba394aac226a135f3830
Sean Purcell <iburinoc@gmail.com>
2016-01-16 12:42:44 -0500

    updated renew script to include both domains

--- renew_cert.sh
+++ renew_cert.sh
@@ -2,4 +2,5 @@
 
 cd /root/letsencrypt/
 ./letsencrypt-auto certonly --webroot -w /var/www/ibsite/ -d seanp.xyz --renew-by-default
+./letsencrypt-auto certonly --webroot -w /var/www/ibsite/ -d www.seanp.xyz --renew-by-default
 service nginx reload
